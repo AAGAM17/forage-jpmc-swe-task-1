1dfd5c0afd7b0625381a48b0fd4d534691001f56
AAGAM SHAH <aagamcshah172005@gmai;.com>
2024-07-14 18:15:24 +0530

    Fix client data feed script and add unit tests

--- client_test.py
+++ client_test.py
@@ -15,10 +15,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+class ClientTest(unittest.TestCase):
+    def test_getDataPoint(self):
+        quote = {
+            'stock': 'ABC',
+            'top_bid': {'price': 120.48, 'size': 10},
+            'top_ask': {'price': 121.2, 'size': 10}
+        }
+        expected = ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2)
+        self.assertEqual(getDataPoint(quote), expected)
 
-
-  """ ------------ Add more unit tests ------------ """
-
+class ClientTest(unittest.TestCase):
+    def test_getRatio(self):
+        price_a = 119.2
+        price_b = 120.48
+        self.assertEqual(getRatio(price_a, price_b), price_a / price_b)
+        self.assertIsNone(getRatio(price_a, 0))
 
 
 if __name__ == '__main__':
